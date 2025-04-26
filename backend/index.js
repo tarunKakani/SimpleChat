3253e712759cc4c43a5cb838543b9f68fa08fe19
tarunKakani <tarunkakani007@gmail.com>
2025-04-26 17:41:15 +0530

    this code snipped commit is not written by me but copied directly as i am not interested in -user is typing- feature

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,49 @@
-import { Server } from 'socket.io'
 import express from 'express'
-import path  from 'path'
+import { Server } from "socket.io"
+import path from 'path'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const app = express()
 const PORT = process.env.PORT || 3500
-const expressServer = app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`)
-})
+
+const app = express()
 
 app.use(express.static(path.join(__dirname, "public")))
 
-const io = new Server(expressServer, {
+const expressServer = app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
 
-    // when you host the frontend, replace the false part (which is actually true) with the frontend hosting address
+const io = new Server(expressServer, {
     cors: {
         origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
-    } 
+    }
 })
 
 io.on('connection', socket => {
     console.log(`User ${socket.id} connected`)
 
+    // Upon connection - only to user 
+    socket.emit('message', "Welcome to Chat App!")
+
+    // Upon connection - to all others 
+    socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)}} connected`)
+
+    // Listening for a message event 
     socket.on('message', data => {
         console.log(data)
-        
-        io.emit('message', `${socket.id.substring(0,5)} : ${data}`)
+        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
+    })
+
+    // When user disconnects - to all others 
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)}} disconnected`)
+    })
+
+    // Listen for activity 
+    socket.on('activity', (name) => {
+        socket.broadcast.emit('activity', name)
     })
 })
